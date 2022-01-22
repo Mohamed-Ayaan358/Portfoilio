84da37794694177af70193bbc0512cd0f740b791
Mohamed-Ayaan358 <ayaan35813@gmail.com>
2022-01-22 18:47:29 +0530

    Should be mobile responsive

--- src/intro.js
+++ src/intro.js
@@ -13,7 +13,7 @@ function Intro() {
     <p className="row " style={{color:"#2DD4BF",fontWeight: "bold" ,paddingRight:"10vh" ,paddingTop:"40px" ,fontSize:"40px"}}>
         Hi ! My name is Mohamed Ayaan and I am an aspiring web developer.
     </p></Col>
-    <Col lg={4}>
+    <Col  lg={4}>
     <model-viewer onClick={sayHello} style={{backgroundColor:"black"}} id="model" src="laptop.glb" autorotate camera-controls></model-viewer></Col>
   </Row>
   
